4dd19b55b022e2b1567fd8f7c9a18b767bc65c2a
Mark Tucker <mark@shazaml.com>
2022-05-01 09:50:04 -0700

    Added animal size slot

--- src/components/AskAnimalsComponent.ts
+++ src/components/AskAnimalsComponent.ts
@@ -16,23 +16,33 @@ export class AskAnimalsComponent extends BaseComponent {
             return this.$send('What type of animal?')
         }
 
+        if (!this.$component.data.animalSize) {
+            return this.$send('What size of animal?')
+        }
+
         // if (!this.$component.data.animalCount) {
         //     return this.$send('How many do you have?')
         // }
 
         //all slots filled
-        this.$send(`Congratulations! You have ${this.$component.data.animalCount} ${this.$component.data.animalType}.`);
+        this.$send(`Congratulations! You have ${this.$component.data.animalCount} ${this.$component.data.animalSize} ${this.$component.data.animalType}.`);
 
         return this.$redirect(AskMainMenuComponent);        
     }
 
     AnimalIntent() {
         if (this.$entities.animal) {
+            // use .resolved to pick the canonical value and not the synonyms
             this.$component.data.animalType = this.$entities.animal.resolved;
         }
 
         if (this.$entities.number) {
             this.$component.data.animalCount = this.$entities.number.resolved;
+        }
+
+        if (this.$entities.size) {
+            // use .value to repeat the words that the user spoke
+            this.$component.data.animalSize = this.$entities.size.value;
         }
 
         return this.START();
